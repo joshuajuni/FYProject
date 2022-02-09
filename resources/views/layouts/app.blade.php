566ecc73fd9bc4d2a28ebf5368e38a474dcd3c46
joshua juni <joshua99juni@gmail.com>
2022-02-09 17:20:33 +0800

    layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Dashboard - TEST</title>
+    <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i') }}" rel="stylesheet">
+    <link href="{{ asset('assets/fonts/fontawesome-all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/fonts/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/fonts/fontawesome5-overrides.min.css') }}" rel="stylesheet">
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+        @include('layouts.top_navbar')
+        <div class="d-flex flex-column" id="content-wrapper">
+            <div id="content">
+                @include('layouts.side_navbar')
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </div>
+            <footer class="bg-white sticky-footer">
+                <div class="container my-auto">
+                    <div class="text-center my-auto copyright"><span>Copyright © FCSIT Postgraduate by Research Internal Rigorous Assessment and Proposal Defense System 2022</span></div>
+                </div>
+            </footer>
+        </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/chart.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
+
+</html>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Add Live Bidding')
+
+@section('content')
+
+@endsection
