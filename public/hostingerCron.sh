c403a3c5ebbad8ea6aa04c16ba0fea0f76d00855
joshuajuni <joshua99juni@gmail.com>
2022-06-01 10:11:49 +0800

    cron job update

--- public/hostingerCron.sh
+++ public/hostingerCron.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /home/u150744244/domains/mohamad-imran.com/pdmodule && php artisan schedule:run >> /dev/null 2>&1
+cd /home/u705863901/domains/64803fyproject.tech/FYProject && php artisan schedule:run >> /dev/null 2>&1
